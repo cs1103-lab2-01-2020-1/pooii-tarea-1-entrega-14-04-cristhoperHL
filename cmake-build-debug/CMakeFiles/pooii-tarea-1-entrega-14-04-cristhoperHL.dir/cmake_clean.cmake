cd07401ca55bb6147297eda936eb6684b28fea7f
cristhoperHL <cristhopermma@gmail.com>
2020-04-11 13:33:30 -0500

    update_estructura

---
+++ cmake-build-debug/CMakeFiles/pooii-tarea-1-entrega-14-04-cristhoperHL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pooii-tarea-1-entrega-14-04-cristhoperHL.dir/src/ejercicios.cpp.o"
+  "CMakeFiles/pooii-tarea-1-entrega-14-04-cristhoperHL.dir/src/main.cpp.o"
+  "pooii-tarea-1-entrega-14-04-cristhoperHL.pdb"
+  "pooii-tarea-1-entrega-14-04-cristhoperHL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pooii-tarea-1-entrega-14-04-cristhoperHL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/pooii-tarea-1-entrega-14-04-cristhoperHL.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pooii-tarea-1-entrega-14-04-cristhoperHL.
+# This may be replaced when dependencies are built.
